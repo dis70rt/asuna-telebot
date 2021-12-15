d4df460c751ec4f42d700ef3c6eb5dae898e062f
saikuno <90782477+saikuno@users.noreply.github.com>
2021-12-15 16:22:11 +0530

    cool

--- lib/image.py
+++ lib/image.py
@@ -1,13 +1,19 @@
 import os
 import requests
+from PIL import Image
 
 dirname = os.path.dirname(__file__)
 
 def download(user_id,URL):
-    filename = os.path.join(dirname, f'image\\{user_id}.jpg')
+
+    filename = os.path.join(dirname, f'image\\{user_id}.png')
     image = requests.get(URL).content
+
     with open(filename, 'wb') as handler:
         handler.write(image)
+        picture = Image.open(filename)
+        picture.save(filename, 
+                 optimize = True, 
+                 quality = 1)
     
     return filename
-
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import telebot
 from telebot.types import InlineKeyboardButton
 from lib import wallpaper as WP
-from lib import Data, image
+from lib import image
 import time
 
 os.system('cls' if os.name=='nt' else 'clear')
@@ -12,13 +12,12 @@ API_KEY = os.getenv("API_KEY") if os.getenv("API_KEY")!=None else "2068683845:AA
 bot = telebot.TeleBot(API_KEY)    
  
 wallpaper = telebot.types.InlineKeyboardMarkup()
+wallpaper2 = telebot.types.InlineKeyboardMarkup()
 contact = telebot.types.InlineKeyboardMarkup()
 
 WALLPAPER = {
     "Anime" : "animewallpaper",
-    "Cars" : "ForzaWallpapers",
-    "Games" : "gamewallpaper",
-    "Random" : "wallpaper"
+    "Mobile" : "MobileWallpaper"
 }
 
 items = list(WALLPAPER.keys())
@@ -28,36 +27,21 @@ wallpaper.add(
     InlineKeyboardButton(items[1],callback_data=items[1])
     )
 
-wallpaper.add(
-    InlineKeyboardButton(items[2],callback_data=items[2]),
-    InlineKeyboardButton(items[3],callback_data=items[3])
-    )
-
 contact.add(InlineKeyboardButton("Contact Developer",callback_data="contact"))
 contact.add(InlineKeyboardButton("Explore Wallpaper",callback_data="explore"))
+
+wallpaper2.add(InlineKeyboardButton("Explore Wallpaper",callback_data="explore"))
 
 def sleep(seconds: int):
     start = time.time()
     while (time.time() - start < seconds):
         pass
 
-
 @bot.message_handler(commands=['start'])
 def start(message):
 
-    data = [
-        message.from_user.id,
-        message.from_user.username,
-        message.from_user.first_name,
-        message.from_user.last_name
-    ]
-
-    try:
-        Data.import_data(data)
-    except Exception as e: print(f"Error: {e}")
-
     msg = rf'''
-    Koniciwa!, {data[2]}
+    Koniciwa!, {message.from_user.first_name}
 Nice to meet you 😊
 
 I'm Asuna and I will send you wallpaper as per your interest.
@@ -66,12 +50,6 @@ If you have any suggestions, please feel free to contact my developer
 
     bot.send_photo(message.chat.id, caption=msg, photo=open('pfp.jpg','rb'), reply_markup=contact)
 
-    data = [
-        message.from_user.id,
-        message.from_user.username,
-        message.from_user.first_name,
-        message.from_user.last_name
-    ]
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
@@ -100,6 +78,16 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, text="I only Provide High Quality Wallpaper 😁", reply_markup=wallpaper)
     
     if call.data == 'contact':
-        bot.send_message(call.message.chat.id, text="[Saikat](https://t.me/saikat0326)", parse_mode='MarkdownV2')
-    
+        developer = r"""Letter for the developer:
+
+**Thanks** for using my Bot\.
+I hope, this kills your trouble to find wallpapers\.
+
+If you have any problems or suggestions, you can contact me through [Twitter](twitter.com/saikatdas_) or directly [DM me on Telegram](https://t.me/saikat0326)
+            
+Have a nice day \!
+        """
+        bot.send_message(call.message.chat.id, text=developer, parse_mode='MarkdownV2',
+                            reply_markup=wallpaper2, disable_web_page_preview=True)
+
 bot.polling()
