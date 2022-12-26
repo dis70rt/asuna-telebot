6aa85c0942a02ed20db8adcb39d2fb2bb0274162
Saikat Das <sd.saikat369@gmail.com>
2022-12-26 07:16:48 +0530

    Update wallpaper.py

--- lib/wallpaper.py
+++ lib/wallpaper.py
@@ -1,5 +1,5 @@
-from urllib.request import urlopen
 import praw
+import random
 
 reddit = praw.Reddit(
     client_id="jRqKwE1Z7i7pMo8JdYZ7pA",
@@ -11,13 +11,15 @@ subreddit = reddit.subreddit("animewallpaper")
 top = []
 hot = []
 
+for submission in subreddit.hot(limit=1000):
+    hot.append(submission)
+
+for submission in subreddit.top(limit=1000):
+    top.append(submission)
+
 def wallpaper(TYPE: str):
     if str == "hot":
-        for submission in subreddit.hot(limit=50):
-            hot = lists(submission)
-        return hot[random.randint(0,50)].url
+        return hot[random.randint(0,1000)].url
     else :
-        for submission in subreddit.top(limit=50):
-            top = lists(submission)
-        return top[random.randint(0,50)].url
+        return top[random.randint(0,1000)].url
 
